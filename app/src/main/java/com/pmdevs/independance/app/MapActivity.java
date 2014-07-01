f543403141d4a2812f8b06a2c6408d96f2fa69f8
Power Moves <powermovesdev@gmail.com>
2014-07-01 17:20:09 -0500

    moved geocoding logic to MapView fragment class

--- app/src/main/java/com/pmdevs/independance/app/MapActivity.java
+++ app/src/main/java/com/pmdevs/independance/app/MapActivity.java
@@ -3,21 +3,8 @@ package com.pmdevs.independance.app;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.Toast;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.io.IOException;
-import java.util.List;
 
 /**
  * Created by Administrator on 6/29/2014.
@@ -25,9 +12,8 @@ import java.util.List;
 public class MapActivity extends Activity {
     private MapView mapView;
     private FragmentManager mFragmentMngr;
-    private GoogleMap googleMap;
-    MarkerOptions markerOptions;
-    LatLng latLng;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -40,75 +26,16 @@ public class MapActivity extends Activity {
         fragmentTransaction.add(R.id.fragment_container, mapView);
         fragmentTransaction.commit();
 
-            String newString;
-        if(savedInstanceState==null) {
 
-            Intent in = getIntent();
-            Bundle b = in.getExtras();
-            if (b == null){
-                newString = null;
-            } else {
-                newString = b.getString("ZIP_INFO");
-                new GeocoderTask().execute(newString);
-            }
-        }
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    private class GeocoderTask extends AsyncTask<String, Void, List<Address>>{
 
-        @Override
-        protected List<Address> doInBackground(String... locationName) {
-            // Creating an instance of Geocoder class
-            Geocoder geocoder = new Geocoder(getBaseContext());
-            List<Address> addresses = null;
-
-            try {
-                // Getting a maximum of 3 Address that matches the input text
-                addresses = geocoder.getFromLocationName(locationName[0], 3);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return addresses;
-        }
-        @Override
-        protected void onPostExecute(List<Address> addresses) {
-
-            if(addresses==null || addresses.size()==0){
-                Toast.makeText(getBaseContext(), "No Location found", Toast.LENGTH_SHORT).show();
-            }
-
-            // Clears all the existing markers on the map
-
-
-            // Adding Markers on Google Map for each matching address
-            for(int i=0;i<addresses.size();i++){
-
-                Address address = addresses.get(i);
-
-                // Creating an instance of GeoPoint, to display in Google Map
-                latLng = new LatLng(address.getLatitude(), address.getLongitude());
-
-                String addressText = String.format("%s, %s",
-                        address.getMaxAddressLineIndex() > 0 ? address.getAddressLine(0) : "",
-                        address.getCountryName());
-
-                markerOptions = new MarkerOptions();
-                markerOptions.position(latLng);
-                markerOptions.title(addressText);
-
-                googleMap.addMarker(markerOptions);
-
-                // Locate the first location
-                if(i==0)
-                    googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-            }
-        }
-
-    }
 
 
 }
--- app/src/main/java/com/pmdevs/independance/app/MapView.java
+++ app/src/main/java/com/pmdevs/independance/app/MapView.java
@@ -1,14 +1,81 @@
 package com.pmdevs.independance.app;
 
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Administrator on 6/29/2014.
  */
 public class MapView extends MapFragment {
+    LatLng MAIN_LOCATION = new LatLng(44.9833, -93.2667);
+    public String newString ="1456 West 78th Street, Chanhassen, MN" ;
+    MarkerOptions markerOptions;
+    LatLng latLng;
 
     public MapView() {
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        getMap().setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(MAIN_LOCATION, 8);
+        getMap().animateCamera(update);
+        try {
+            geoLocate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void geoLocate () throws IOException {
+
+
+        Geocoder gc = new Geocoder(getActivity());
+
+        List<Address> list = gc.getFromLocationName(newString, 1);
+
+
+
+        Address add = list.get(0);
+        String locality = add.getLocality();
+        Toast.makeText(getActivity(), locality, Toast.LENGTH_LONG).show();
+
+
+
+        latLng = new LatLng(add.getLatitude(), add.getLongitude());
+        CameraUpdate update2 = CameraUpdateFactory.newLatLngZoom(latLng, 8);
+        getMap().animateCamera(update2);
+
+        String addressText = String.format("%s, %s",
+                add.getMaxAddressLineIndex() > 0 ? add.getAddressLine(0) : "",
+                add.getCountryName());
+
+        markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title(addressText);
+
+
+
+        getMap().addMarker(markerOptions);
+
+        // Locate the first location
+        if(add==null)
+            getMap().animateCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+
 }
+
